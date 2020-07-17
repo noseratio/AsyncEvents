640c83ffe7fbbccd7a1d6f0aa086be8a07f55851
Andrew Nosenko <noseratio@gmail.com>
2020-07-17 18:20:15 +1000

    Refactored EventChannel.cs

---
+++ EventChannel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace AsyncEvents
+{
+    /// <summary>
+    /// Wraps events as IAsyncEnumerable asynchronous stream
+    /// </summary>
+    public class EventChannel<T> : IDisposable
+    {
+        private readonly Channel<T> _channel = Channel.CreateUnbounded<T>();
+
+        public Task Completion => _channel.Reader.Completion;
+
+        /// <summary>Queue an event item to the write side of the channel</summary>
+        public bool Post(T item)
+        {
+            return _channel.Writer.TryWrite(item);
+        }
+
+        /// <summary>Read queued events as an async stream</summary>
+        public IAsyncEnumerable<T> ToAsyncEnumerable(CancellationToken token)
+        {
+            return _channel.Reader.ReadAllAsync(token);
+        }
+
+        /// <summary>A simple helper to wrap even handler scope as IDisposable</summary>
+        internal struct EventSubscription<TEventHandler> : IDisposable
+            where TEventHandler : Delegate
+        {
+            private readonly Action _unsubscribe;
+
+            public EventSubscription(
+                TEventHandler handler,
+                Action<TEventHandler> subscribe,
+                Action<TEventHandler> unsubscribe)
+            {
+                subscribe(handler);
+                _unsubscribe = () => unsubscribe(handler);
+            }
+
+            public void Dispose()
+            {
+                _unsubscribe();
+            }
+        }
+
+        /// <summary>
+        /// Subscribe to an event
+        /// </summary>
+        public IDisposable Subscribe<TEventHandler>(
+            TEventHandler handler,
+            Action<TEventHandler> subscribe,
+            Action<TEventHandler> unsubscribe) where TEventHandler : Delegate
+        {
+            return new EventSubscription<TEventHandler>(handler, subscribe, unsubscribe);
+        }
+
+        public void Dispose()
+        {
+            _channel.Writer.Complete();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,68 +10,6 @@ using System.Windows.Forms;
 
 namespace AsyncEvents
 {
-    /// <summary>
-    /// Wraps events as IAsyncEnumerable asynchronous stream
-    /// </summary>
-    public class EventChannel<T> : IDisposable
-    {
-        private readonly Channel<T> _channel = Channel.CreateUnbounded<T>();
-
-        public Task Completion => _channel.Reader.Completion;
-
-        /// <summary>Queue an event item to the write side of the channel</summary>
-        public bool Post(T item)
-        {
-            return _channel.Writer.TryWrite(item);
-        }
-
-        /// <summary>Read queued events as an async stream</summary>
-        public IAsyncEnumerable<T> ToAsyncEnumerable(CancellationToken token)
-        {
-            return _channel.Reader.ReadAllAsync(token);
-        }
-
-        /// <summary>A simple helper to wrap even handler scope as IDisposable</summary>
-        internal struct EventSubscription<TEventHandler> : IDisposable
-            where TEventHandler : Delegate
-        {
-            private readonly Action _unsubscribe;
-
-            public EventSubscription(
-                TEventHandler handler,
-                Action<TEventHandler> subscribe,
-                Action<TEventHandler> unsubscribe)
-            {
-                subscribe(handler);
-                _unsubscribe = () => unsubscribe(handler);
-            }
-
-            public void Dispose()
-            {
-                _unsubscribe();
-            }
-        }
-
-        /// <summary>
-        /// Subscribe to an event
-        /// </summary>
-        public IDisposable Subscribe<TEventHandler>(
-            TEventHandler handler,
-            Action<TEventHandler> subscribe,
-            Action<TEventHandler> unsubscribe) where TEventHandler : Delegate
-        {
-            return new EventSubscription<TEventHandler>(handler, subscribe, unsubscribe);
-        }
-
-        public void Dispose()
-        {
-            _channel.Writer.Complete();
-        }
-    }
-
-    /// <summary>
-    /// The UI and ReadEventStreamAsync
-    /// </summary>
     class Program
     {
         /// <summary>process the async stream of events</summary>
